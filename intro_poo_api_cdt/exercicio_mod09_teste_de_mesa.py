82f0d081bc3fcfb5a01eb3f8bd4f340c15483b71
Rychard Rodrigues <rychardrdsilva19@gmail.com>
2026-04-27 09:50:58 -0300

    Add files via upload

---
+++ intro_poo_api_cdt/exercicio_mod09_teste_de_mesa.py
@@ -0,0 +1,20 @@
+# Aqui usamos o 'try' para converter uma duração (texto) em números para calcular o gasto de bateria.
+
+class Celular:
+    def __init__(self, marca, modelo):
+        self.marca, self.modelo = marca, modelo
+        self.bateria = 100
+
+    def fazer_chamada(self, duracao):
+        try:
+            gasto = int(duracao) * 2
+            if self.bateria >=gasto:
+                self.bateria -= gasto
+                print(f"Chamada de {duracao} min feita! Bateria: {self.bateria}%")
+            else:
+                print("Bateria insuficiente!")
+        except ValueError:
+            print("Erro: A duração deve ser um número inteiro!")
+            
+meu_celular = Celular("Samsung", "S24")
+meu_celular.fazer_chamada("dez")
---
+++ intro_poo_api_cdt/explicação-mod09.py
@@ -0,0 +1,19 @@
+# Neste código, aprendemos a capturar o erro de dividir por zero e o erro de digitar letras.
+
+def aula_tratamento_de_erros():
+    print("---Desafio 1: Divisão---")
+    try:
+        numerador = int(input("Digita o numerador:"))
+        denominador = int(input("Digita o denominador:"))
+        resultado = numerador / denominador
+    except ValueError:
+        print("Erro: Digita apenas números inteiros!")
+    except ZeroDivisionError:
+        print("Erro: Não podes dividir por zero!")
+    except Exception as erro:
+        print(f"Erro inesperado: {erro}")
+    else:
+        print(f"Sucesso! Resultado: {resultado}")
+    finally:
+        print("---Fim da Divisão---")
+aula_tratamento_de_erros()
